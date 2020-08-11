1de3af447dd9671e8d64a1e30ad2ddf5484d36fb
kinming92 <kinming.klm@gmail.com>
2020-08-11 16:05:04 -0700

    proof read and fix the route that works with gh pages

--- src/Component/App.js
+++ src/Component/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         image: "/image/movie-collection-backend.png",
         title: "Movie Collection Website Backend Rest API",
         description: `A node.js RESTful app is to serve as endpoint for the simple movie collection website. 
-                      Technologies and tools used include Node.js, Express.js, MongoDB, Mongoose, JavaScript ECMAScript 6, JsonWebToken`,
+                      Technologies and tools used include Node.js, Express.js, MongoDB, Mongoose, JavaScript ECMAScript 6, JsonWebToken.`,
         doc: "https://github.com/kinming92/movie-website-rest-api",
         demo: ""
       },
@@ -37,7 +37,7 @@ class App extends Component {
         id: 1,
         image: "/image/movie-collection-frontend.png",
         title: "Movie Collection Website Frontend",
-        description: "A simple website that allows user to create, read update and delete movie listing. I have worked on retrieving the user’s data from database using Asynchronous JavaScript and XML (AJAX). I implemented a simple responsive page for user login, signup and information update. Technologies and tools used include HTML5, CSS, JavaScript, AJAX, GIT",
+        description: "A simple website that allows user to create, read, update and delete movie listing. I have worked on retrieving the user’s data from database using Asynchronous JavaScript and XML (AJAX). I implemented a simple responsive page for user login, signup and information update. Technologies and tools used include HTML5, CSS, JavaScript, AJAX, and GIT.",
         doc: "https://github.com/kinming92/movie-collection-website",
         demo: "https://movie-collection-frontend.web.app/"
       },
@@ -45,7 +45,7 @@ class App extends Component {
         id: 2,
         image: "/image/machine-learning-art.png",
         title: "Contour Transformation Machine Learning Art Project",
-        description: "I explored Pix2pix, Holistically-Nested Edge(HED) machine learning model. Technologies and Tools Used includes Python3, Jupter Notebook, Pix2pix model, Holistically-Nested Edge(HED) model",
+        description: "I explored the Pix2pix and the Holistically-Nested Edge(HED) machine learning model. Technologies and Tools used include Python3, Jupter Notebook, Pix2pix model, and Holistically-Nested Edge(HED) model.",
         doc: "https://github.com/ucsd-ml-arts/ml-art-final2-amcda-anymlcandoart",
         demo: ""
       },
@@ -53,7 +53,7 @@ class App extends Component {
         id: 3,
         image: "/image/9-bit-ISA.png",
         title: "9 Bit Instruction Architeture Set",
-        description: "The goal of this project was to build a hardware stimulation to solve simple Math problems which includes multiplication and long division. We designed a new set of assembly set using the accumulator method that handle the 9 bit instruction. We designed the datapath and the control logic of the hardware System Verilog module. SystemVerilog Modules were implemented based the diagram above. Each module was unit-tested individually to ensure that we got the correct output. Quartus was used to generate the schematic.",
+        description: "The goal of this project was to build a hardware stimulation to solve simple Math problems which include multiplication and long division. We designed a new set of assembly set using the accumulator method that handles the 9 bit instruction. We designed the datapath and the control logic of the hardware with SystemVerilog module. SystemVerilog Modules were then implemented based on the diagram above. Each module was unit-tested individually to ensure that we got the correct output. Quartus was used to generate the schematic.",
         doc: "https://github.com/kinming92/Divvy_FinalProject",
         demo: ""
       },
@@ -61,7 +61,7 @@ class App extends Component {
         id: 4,
         image: "/image/Simplified-MIPS-Processor-Architecture.png",
         title: "MISP Processor with Coprocessor 1 project ",
-        description: "I assisted in building a single clock cycle MIPS processor and Coprocessor 1, Floating Point Unit. I tested the output correctness with SystemVerilog testbench using Vivado",
+        description: "I assisted in building a single clock cycle MIPS processor and Coprocessor 1, Floating Point Unit. I tested the output correctness with SystemVerilog testbench using Vivado.",
         doc: "https://github.com/kinming92/MISP-FPU-Processor",
         demo: ""
       },
@@ -106,7 +106,7 @@ class App extends Component {
               <Route exact path='/react-portfolio' component={ lang === 'en' ? HomeEn : HomeCh } />
               <Route path='/react-portfolio/contact' component={ lang === 'en' ? ContactEn : ContactCh } />
               <Route path='/react-portfolio/about' component={ lang === 'en' ? AboutEn : AboutCh } />
-              <Route path={process.env.PUBLIC_URL + '/projects'} render={() => project}/>
+              <Route path='/react-portfolio/projects' render={() => project}/>
               
               {/* use button / iframe */}
               <Route path='/github' component={({history}) => { 
